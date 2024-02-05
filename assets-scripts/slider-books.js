9cb62fb76e646142341e16c91c87390ce26585a8
Wesley Medeiros <139824516+DevWesleyMedeiros@users.noreply.github.com>
2024-02-05 16:32:59 -0300

    Projeto em andamento

--- assets-scripts/slider-books.js
+++ assets-scripts/slider-books.js
@@ -1,4 +1,4 @@
-const containerRecomendations = document.querySelector(".container-recomendations")
+const containerRecomendations = document.querySelector(".container-recomendations");
 const containerRecomendationBooks = document.querySelector(".container-recomendation-books");
 const icons = [...document.querySelectorAll(".icon")];
 
@@ -6,33 +6,40 @@ let sliderIndex = 0;
 
 let divsBookSingle = 10;
 for (let d = 0; d < divsBookSingle; d++) {
-    createDiv();
+  createDiv();
 }
+
+let GridTemplateColumns = `repeat(${divsBookSingle}, 1fr)`;
+containerRecomendationBooks.style.gridTemplateColumns = GridTemplateColumns;
 
 function createDiv() {
   let div = document.createElement("div");
   div.setAttribute("class", "book-single");
   containerRecomendationBooks.appendChild(div);
-  containerRecomendations.lastElementChild.before(containerRecomendationBooks);
   return containerRecomendations;
 }
+
 moveSlider();
 
 function moveSlider() {
   icons.forEach((icon) => {
+    if (icon.id === "right") {
+      icon.style.display = "block";
+    }else if (icon.id === "left"){
+      icon.style.display = divsBookSingle > 5 ? "block" : "none";
+    }
     icon.addEventListener("click", function () {
       const sliders = [...document.querySelectorAll(".book-single")];
       const numSliders = sliders.length;
 
       if (icon.id === "left") {
-        sliderIndex = Math.max(0, sliderIndex - (numSliders % 5 === 0 ? 5 : 1));
+        sliderIndex = Math.max(0, sliderIndex - (numSliders % 5 === 0 ? 1 : 1));
       } else if (icon.id === "right") {
-        sliderIndex = Math.min(numSliders - 5, sliderIndex + (numSliders % 5 === 0 ? 3 : 1));
+        sliderIndex = Math.min(numSliders - 5, sliderIndex + (numSliders % 5 === 0 ? 1 : 1));
       }
-
       containerRecomendationBooks.scrollTo({
         left: sliders[sliderIndex].offsetLeft,
-        behavior: "smooth",
+        behavior: "smooth"
       });
     });
   });
