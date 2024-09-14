80eb83417558f1e8b2f6a89c553f194a9bbac4fc
Wesley <DevWesleyMedeiros@yahoo.com>
2024-09-14 17:13:58 -0300

    Script da API em andamento

--- API-call/index.ts
+++ API-call/index.ts
@@ -1,100 +1,56 @@
-import { axios } from "axios";
-// AQUI VIRÁ OS SCRIPTS PARA CONSUMO DAS APIS DO GOOGLE E DO OPEN LIBRARY
+import axios from "axios";
 
-//  function nome():void{
-//     window.alert("Wesley");
-//  }
-//  nome();
-// LINK QUE PESQUISA PELO NOME DO AUTOR;
-// const endingPointAuthorSearch = "https://openlibrary.org/search/authors.json?q=robert kiyosaki";
+class AllApiEndingPoints {
+    private bookInformation: string;
+    private endingPoint: string;
 
-// function searchForAuthor(){
-//     axios.get(endingPointAuthorSearch)
-//   .then(function (response) {
-//     console.log(response);
-//     console.log(response.data.docs[0].top_work);
-//   })
-//   .catch(function (error) {
-//     console.error(error);
-//   })
-//   .finally(function () {
-//     console.log("Requisição finalizada")
-//   });
-// }
-// console.log(searchForAuthor());
+    constructor(bookInformation: string) {
+        this.bookInformation = bookInformation;
+        this.endingPoint = `https://openlibrary.org/search.json?q=${this.handleInputLink(this.bookInformation)}`;
+    }
 
-// // PEGAR A UMA EDIÇÃO DE UM LIVRO (edition_key)
-// const endingPointForEdition = "https://openlibrary.org/books/works/OL2010879W";
+    protected handleInputLink(query: string): string {
+        const splitText: string[] = query.split(" ");
+        const joinText: string = splitText.join("+");
+        return joinText;
+    }
 
-// function getEdition() {
-//     axios.get(endingPointForEdition)
-//     .then(function (response){
-//         console.log(response);
-//     })
-//     .catch(function (error){
-//         console.error("Erro na requisição " + error);
-//     })
-//     .finally(function (){
-//         console.log("Requisição finalizada");
-//     })
-// }
-// console.log(getEdition());
-// function getCover(coverId) {
-//     const endingPointForBookCover = `https://covers.openlibrary.org/a/olid/${coverId}-M.jpg`;
-//     let img = document.createElement("img");
-//     img.src = endingPointForBookCover;
-//     document.body.appendChild(img);
-// }
+    private showInformation(): void {
+        if (this.endingPoint) {
+            axios.get(this.endingPoint)
+                .then(response => {
+                    console.log(response.data); 
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        } else {
+            console.error("Ending point is not defined");
+        }
+    }
 
+    public fetchInformation(): void {
+        this.showInformation();
+    }
+}
 
-// ABAIXO O LINK QUE PESQUISA LIVRO PELO TÍTULO
-// const buttonCall = document.querySelector(".search");
-// const divToString = document.getElementById("toString");
+document.addEventListener('DOMContentLoaded', () => {
+    const searchInput = document.querySelector("input[type=search]") as HTMLInputElement;
+    const buttonSearch = document.querySelector(".search-icon");
 
-// const splitString = (string) => {
-//     let splitText = string.split(" ");
-//     let joinText = splitText.join("+");
-//     return joinText;
-// }
+    if (searchInput && buttonSearch) {
+        buttonSearch.addEventListener("click", () => {
+            const inputSearchValue = searchInput.value.toUpperCase();
+            const api = new AllApiEndingPoints(inputSearchValue);
+            api.fetchInformation();
+        });
+    } else {
+        console.error("Search input or button not found");
+    }
+});
 
-// function getBookByTitle() {
-//     const inputUrl = document.querySelector("input[type=url]").value.trim().toUpperCase();
-//     if (!inputUrl) {
-//         window.alert("Insira o nome do livro");
-//         document.querySelector("input[type=url]").focus();
-//         return;
-//     }
-    
-//     const endingPointBookTitle = `https://openlibrary.org/search.json?title=${splitString(inputUrl)}`;
-//     axios.get(endingPointBookTitle)
-//     .then(function (response) {
-//         console.log(response.data.docs[0]);
-        
-//         if (response.data.docs.length > 0) {
-//             let ArrayHeader = {
-//                 titulo: response.data.docs[0].title || "Título não disponível",
-//                 subtitulo: response.data.docs[0].subtitle || "Subtítulo não disponível",
-//                 autor: response.data.docs[0].author_name ? response.data.docs[0].author_name.join(", ") : "Autor não disponível",
-//                 data_publicacao: response.data.docs[0].first_publish_year || "Data de publicação não disponível",
-//                 numero_de_paginas: response.data.docs[0].number_of_pages_median || "Número de páginas não disponível",
-//                 temas_do_livro: response.data.docs[0].subject ? response.data.docs[0].subject.join(", ") : "Temas não disponíveis"
-//             };
-            
-//             divToString.innerText = "";
+class ApiForAutors extends AllApiEndingPoints {}
+class ApiForBookTitle extends AllApiEndingPoints {}
+class ApiForBookCover extends AllApiEndingPoints {}
+class ApiForGeneralInformationAutor extends AllApiEndingPoints {}
 
-//             for (let key in ArrayHeader) {
-//                 divToString.innerText += `${key}: ${ArrayHeader[key]}\n`;
-//             }
-//         } else {
-//             divToString.innerText = "Nenhum livro encontrado.";
-//         }
-//     })
-//     .catch(function (error) {
-//         console.error(error);
-//     })
-//     .finally(function () {
-//         console.log("Requisição finalizada");
-//     });
-// }
-
-// buttonCall.addEventListener("click", getBookByTitle);
